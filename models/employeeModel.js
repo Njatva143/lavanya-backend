a33833cabc0b98d6d8c0149754e3da125991bfa3
Njatva143 <njatva143@gmail.com>
2025-04-17 10:52:16 +0530

    Create employeeModel.js

---
+++ models/employeeModel.js
@@ -0,0 +1,9 @@
+// models/employeeModel.js
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  position: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Employee', employeeSchema);
