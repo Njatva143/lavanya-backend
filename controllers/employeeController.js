bb3f16d35d3115252eb7bc9b69ae37f0d024a072
Njatva143 <njatva143@gmail.com>
2025-04-17 15:55:01 +0530

    Add files via upload

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,24 +1,10 @@
-// controllers/employeeController.js
-const Employee = require('../models/employeeModel');
+const Employee = require("../models/Employee");
 
-// Get all employees
-exports.getAllEmployees = async (req, res) => {
+exports.getEmployees = async (req, res) => {
   try {
     const employees = await Employee.find();
     res.json(employees);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Add a new employee
-exports.addEmployee = async (req, res) => {
-  const { name, position } = req.body;
-  try {
-    const newEmployee = new Employee({ name, position });
-    await newEmployee.save();
-    res.status(201).json(newEmployee);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
